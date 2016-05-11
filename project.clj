1bda81cd60eb0360885cc6d5c12fe5bd3daec081
Chris Wolfe <chriswwolfe@gmail.com>
2016-05-11 17:31:26 -0500

    release should *not* be 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fernet "1.0.0-SNAPSHOT"
+(defproject fernet "0.3.0-SNAPSHOT"
   :description "Authenticated symmetric encryption made easy."
   :url "https://github.com/derwolfe/fernet-clj"
   :license {:name "MIT"
