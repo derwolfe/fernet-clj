32b1c571fb9ac93dc3ae9a906451d1b742195bc6
Chris Wolfe <chriswwolfe@gmail.com>
2016-12-03 10:26:34 -0600

    add some hints, see if execution speeds up

--- src/fernet/frame.clj
+++ src/fernet/frame.clj
@@ -7,9 +7,9 @@
   bp/BuffyType
   (size [_] 1)
   (write [bt buffer idx value]
-    (.setByte buffer idx value))
+    (.setByte ^io.netty.buffer.UnpooledHeapByteBuf buffer idx value))
   (read [by buffer idx]
-    (bit-and 0xFF (.getByte buffer idx))))
+    (bit-and 0xFF (.getByte ^io.netty.buffer.UnpooledHeapByteBuf buffer idx))))
 
 (def ubyte-type (memoize #(UByteType.)))
 
@@ -27,7 +27,7 @@
     (bc/compose-buffer spec :buffer-type :heap)))
 
 (defn fill-buffer [buf b]
-  (.setBytes (bc/buffer buf) 0 b)
+  (.setBytes ^io.netty.buffer.UnpooledHeapByteBuf (bc/buffer buf) 0 ^bytes b)
   buf)
 
 (defn get-bytes
@@ -35,11 +35,12 @@
     (get-bytes buf 0 length))
   ([buf start length]
    (let [b (byte-array length)]
-     (.getBytes buf start b 0 length)
+     ;; hmm, these hints seem off
+     (.getBytes ^io.netty.buffer.UnpooledHeapByteBuf buf ^long start ^bytes b 0 ^long length)
      b)))
 
 (defn encode-token
-  [{:keys [version ciphertext iv timestamp hmac-fn]}]
+  [{:keys [version ^bytes ciphertext iv timestamp hmac-fn]}]
   (let [ciphertext-length (alength ciphertext)
         signed-length (- (+ overhead ciphertext-length) 32)
         buf (token-buf ciphertext-length)]
@@ -48,10 +49,10 @@
                      :iv iv
                      :ciphertext ciphertext})
     (bc/set-field buf :hmac (hmac-fn (get-bytes (bc/buffer buf) signed-length)))
-    (.array (bc/buffer buf))))
+    (.array ^io.netty.buffer.UnpooledHeapByteBuf (bc/buffer buf))))
 
 (defn decode-token
-  [b]
+  [^bytes b]
   (let [signed-length (- (alength b) 32)
         buf (token-buf (- (alength b) overhead))]
     (fill-buffer buf b)
